27587100cab22abc72bd6675804fc576662b8662
wooky <raggamuffin14@gmail.com>
2018-07-06 12:15:45 +0200

    poprawione controllery i exception

--- src/main/java/pl/gametime/Controller/PlayerController.java
+++ src/main/java/pl/gametime/Controller/PlayerController.java
@@ -1,10 +1,12 @@
 package pl.gametime.Controller;
 
 import org.springframework.web.bind.annotation.*;
+import pl.gametime.Exception.ResourceNotFoundException;
 import pl.gametime.Model.Player;
 import pl.gametime.Repo.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -23,6 +25,31 @@ public class PlayerController {
     public List<Player> getAll(){
         return playerRepository.findAll();
 
+    }
+    @RequestMapping("/findplayerbyid")
+    public String findById(@RequestParam("id") long id)
+    {
+        String result= "";
+        result = playerRepository.findById(id).toString();
+        return result;
+    }
+
+    @RequestMapping("/deleteplayerbyid")
+
+    public void deleteById(@RequestParam("id") long id)
+    {
+        playerRepository.deleteById(id);
+    }
+    @PutMapping("/players/{player_id}")
+    public Player updatePlayer(@PathVariable Long player_id,
+                                   @Valid @RequestBody Player player) {
+        return playerRepository.findById(player_id).map(editedPlayer -> {
+                    player.setFirstname(player.getFirstname());
+                    player.setLastname(player.getLastname());
+                    player.setClubname(player.getClubname());
+                    player.setSalary(player.getSalary());
+                    return playerRepository.save(editedPlayer);
+                }).orElseThrow(() -> new ResourceNotFoundException("Player not found with id " + player_id));
     }
 
     @PostMapping("/addplayer")
--- src/main/java/pl/gametime/Controller/TeamController.java
+++ src/main/java/pl/gametime/Controller/TeamController.java
@@ -1,9 +1,13 @@
 package pl.gametime.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.gametime.Exception.ResourceNotFoundException;
+import pl.gametime.Model.Team;
 import pl.gametime.Repo.TeamRepository;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class TeamController {
@@ -17,5 +21,40 @@ public class TeamController {
         return "tu druzyna";
     }
 
+    @GetMapping("/getallTeams")
+    public List<Team> getAll(){
+        return teamRepository.findAll();
+
+    }
+    @RequestMapping("/findteambyid")
+    public String findById(@RequestParam("id") long team_id)
+    {
+        String result= "";
+        result = teamRepository.findById(team_id).toString();
+        return result;
+    }
+
+    @RequestMapping("/deteleteambyid")
+
+    public void deleteById(@RequestParam("id") long id)
+    {
+        teamRepository.deleteById(id);
+    }
+    @PutMapping("/teams/{team_id}")
+    public Team updateTeam(@PathVariable Long team_id,
+                               @Valid @RequestBody Team team) {
+        return teamRepository.findById(team_id).map(editedTeam -> {
+            team.setClubname(team.getClubname());
+            team.setHometown(team.getHometown());
+            team.setConference(team.getConference());
+            return teamRepository.save(editedTeam);
+        }).orElseThrow(() -> new ResourceNotFoundException("Team not found with id " + team_id));
+    }
+
+    @PostMapping("/addTeam")
+    public Team createTeam(@RequestBody Team team)
+    {
+        return teamRepository.save(team);
+    }
 
 }
